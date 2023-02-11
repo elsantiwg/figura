db9a821c62e3eedf69e0fa7d5919f3eac77f1dab
elsantiwg <kspg2350@gmail.com>
2023-02-10 20:45:51 -0500

    Create Ejercicio.java
    main del ejercicio

---
+++ Ejercicio.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio;
+
+
+/**
+ *
+ * @author kspg2
+ */
+public class Ejercicio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // TODO code application logic here
+      System.out.println("      //\\\\\n     //  \\\\\n    //    \\\\\n================\n================\n     0    0     \n\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\n''''''''''''''''");
+    }
+
+}
